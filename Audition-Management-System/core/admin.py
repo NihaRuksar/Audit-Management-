e467ce3903e885c7cca73efe44a2ede53b528bca
NihaRuksar <146099981+NihaRuksar@users.noreply.github.com>
2025-12-03 14:54:41 +0530

    Add files via upload

---
+++ Audition-Management-System/core/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import FinalPi, Student, TaskRoundOne, TaskRoundTwo, GroupDiscussionRound, PenPaperRound, AppConfig
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'stopped', 'current_round', 'year', 'dept')
+
+
+admin.site.register(Student, StudentAdmin)
+admin.site.register(TaskRoundOne)
+admin.site.register(TaskRoundTwo)
+admin.site.register(GroupDiscussionRound)
+admin.site.register(PenPaperRound)
+admin.site.register(FinalPi)
+admin.site.register(AppConfig)
